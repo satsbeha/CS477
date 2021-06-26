8c6906def4673b16cd2d43c1245f9530cf613dd6
sol68 <solmulesoft46@gmail.com>
2021-06-25 21:37:09 -0500

    lab2

--- LAB2/Excercise1.js
+++ LAB2/Excercise1.js
@@ -2,3 +2,8 @@
 Create a simple Node script that converts 'www.miu.edu' domain name to the equivalent IP address. 
 (Search and learn 'dns' module, resolve4)
 */
+const dns = require('dns');
+dns.resolve4('www.miu.edu', function (error, address) {
+    if (error) throw error;
+    console.log(address);
+});
