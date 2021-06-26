edabfd5b448bcba5e6562fc15e9a6c3364dd91c9
sol68 <solmulesoft46@gmail.com>
2021-06-25 21:21:40 -0500

    lab2

---
+++ LAB2/Excercise1.js
@@ -0,0 +1,4 @@
+/*
+Create a simple Node script that converts 'www.miu.edu' domain name to the equivalent IP address. 
+(Search and learn 'dns' module, resolve4)
+*/
---
+++ LAB2/Excercise3.js
@@ -0,0 +1,5 @@
+/*
+
+Using Node Stream API, create a script to unzip a file (after you zip it). 
+(Use zlib.createGunzip() stream)
+*/
